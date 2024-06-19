b0b4b60fc056b3fdec0c1aeba2ed39cf8900679b
Finlay Neal <finlayhormann@gmail.com>
2024-06-20 09:59:10 +1200

    create table migration file for users

--- server/db/migrations/20240619032036_project.js
+++ server/db/migrations/20240619032036_project.js
@@ -2,7 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export const up = function(knex) {
+    knex.schema.createTable('project', (Table) => {
+        Table.integer('id').primary()
+        Table.string('project_name')
+        Table.integer('owner_id') //Ref: > users.id
+        Table.json('contributer_id') // {"array" : [1,2,3,4,5,6,67,7,3]}
+    })
   
 };
 
@@ -10,6 +16,6 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+export const down = function(knex) {
+    knex.schema.dropTable('project')
 };
