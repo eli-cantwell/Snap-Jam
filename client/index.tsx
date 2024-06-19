2ed3d75ffb012be7845cd11783c6dbd2f4a15d3b
Finlay Neal <finlayhormann@gmail.com>
2024-06-20 09:29:20 +1200

    setup auth client

--- client/index.tsx
+++ client/index.tsx
@@ -15,11 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
      * TODO: replace domain, clientId, and audience
      */
     <Auth0Provider
-      domain=""
-      clientId=""
+      domain="piwakawaka-2024-finlay.au.auth0.com"
+      clientId="FXbIRAWKZM1u4u6XV3WDJw9MGtYNeaUB"
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: '',
+        audience: 'https://snapJam/api',
       }}
     >
       <QueryClientProvider client={queryClient}>
--- server/auth0.ts
+++ server/auth0.ts
@@ -5,8 +5,8 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
-const audience = 'https://'
+const domain = 'https://snapJam/api'
+const audience = 'https://snapJam/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
