98ef5071e8d20ce7a7648e34b7b44ddbc772f2f8
Finlay Neal <finlayhormann@gmail.com>
2024-06-21 09:34:06 +1200

    stash sinlgeProject

--- client/components/SingleProject.tsx
+++ client/components/SingleProject.tsx
@@ -1,3 +1,30 @@
-export default function SingleProject() {
-  return <h1>SingleProject</h1>
- } 
+import { useAudio, useProject } from "../hooks/useUsers"
+
+interface Props {
+  id: number
+}
+
+export default function SingleProject(props: Props) {
+
+  const projectHook = useProject()
+  const audioHook = useAudio()
+
+  const {data: projects, isPending, isError, error} = projectHook.getProjectById(props.id)
+  
+  // const {data: audio } = audioHook.getAudioById() //TODO getAudioByProjectId
+
+
+  isPending && <p>Loading...</p>
+
+  if (isError) {
+    console.log({message: error})
+    return (<p>There was an error: {`${error}`}</p>)
+    }
+
+    return (
+      <>
+        <p>{projects?.project_name}</p>
+      </>
+    )
+
+}
