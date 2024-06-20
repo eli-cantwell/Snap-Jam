2c82f7e3f771ab9df2d3537edfa82f8ae3adfc15
Finlay Neal <finlayhormann@gmail.com>
2024-06-20 15:36:30 +1200

    add project db routes

--- server/routes/project-routes.ts
+++ server/routes/project-routes.ts
@@ -1,7 +1,62 @@
+import { Router } from 'express'
+import checkJwt, { JwtRequest } from '../auth0.ts'
+import { StatusCodes } from 'http-status-codes'
+
+import * as db from '../db/db.ts'
+
+const router = Router()
+export default router
+
 // ALL
+router.get('/', checkJwt, async (req: JwtRequest, res) => {
+    if (!req.auth?.sub) {
+        res.sendStatus(StatusCodes.UNAUTHORIZED)
+        return
+      }
+    try {
+        const data = db.getAllProjects()
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 
+router.get('/getdevprojects', checkJwt, async (req, res) => {
+    try {
+        const data = db.getAllProjects()
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 // ID
+router.get('/:id', checkJwt, async (req: JwtRequest, res) => {
+    const { id } = req.params
+    if (!req.auth?.sub) {
+        res.sendStatus(StatusCodes.UNAUTHORIZED)
+        return
+      }
+    try {
+        const data = db.getProjectsById(Number(id))
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 
+router.get('/getdevprojects/:id', checkJwt, async (req, res) => {
+    const { id } = req.params
+    try {
+        const data = db.getProjectsById(Number(id))
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 // UPDATE
 
 // CREATE
