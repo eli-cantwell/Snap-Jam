24cf1e77d3e30cbcad7857ab250a7c1d0803e0f7
Finlay Neal <finlayhormann@gmail.com>
2024-06-20 15:42:44 +1200

    create db routes

--- server/routes/comment-routes.ts
+++ server/routes/comment-routes.ts
@@ -1,6 +1,60 @@
-// ALL
+import { Router } from 'express'
+import checkJwt, { JwtRequest } from '../auth0.ts'
+import { StatusCodes } from 'http-status-codes'
 
+import * as db from '../db/db.ts'
+
+const router= Router()
+// ALL
+router.get('/', checkJwt, async (req: JwtRequest, res) => {
+    if (!req.auth?.sub) {
+        res.sendStatus(StatusCodes.UNAUTHORIZED)
+        return
+      }
+    try {
+        const data = db.getAllComments()
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
+
+router.get('/getdevcomments', checkJwt, async (req, res) => {
+    try {
+        const data = db.getAllComments()
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 // ID
+router.get('/:id', checkJwt, async (req: JwtRequest, res) => {
+    const { id } = req.params
+    if (!req.auth?.sub) {
+        res.sendStatus(StatusCodes.UNAUTHORIZED)
+        return
+      }
+    try {
+        const data = db.getCommentById((Number(id)))
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
+
+router.get('/getdevcomments/:id', checkJwt, async (req, res) => {
+    const { id } = req.params
+    try {
+        const data = db.getCommentById((Number(id)))
+        res.json (data)
+    }
+    catch (e) {
+        res.json({"message" : `${e}`})
+    }
+})
 
 // UPDATE
 
