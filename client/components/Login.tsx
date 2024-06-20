2675edb5617c6ee66b614864fd225663605ce762
Eli Cantwell <elicantwellpersonal@gmail.com>
2024-06-20 14:53:53 +1200

    complete component wireframe

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -3,6 +3,9 @@ export default function Login() {
     <div className="login-screen bg-green-500  m-auto">
       <h2>Create an account</h2>
       <p>Enter your email to sign up / log in</p>
+    
     </div>
   )
 }
+
+//
