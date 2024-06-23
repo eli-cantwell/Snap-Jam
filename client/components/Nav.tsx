643aecd67d1fed411597a103dece95e71dffc281
Finlay Neal <finlayhormann@gmail.com>
2024-06-23 12:14:54 +1200

    feat: added header but unsurea

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -19,25 +19,31 @@ export default function Nav() {
   // }
   
   return (
-    <div className="nav-bar">
-      <div className="nav-title-div">
-        <h1 className='text-slate-700 font-bold xl'>Snap Jam</h1>
+    <>
+    <div className='relative w-full h-full'>
+      <div className='bg-[url("/images/banner-1563536_1920.jpg")] h-48 w-fill bg-cover border-slate-300 border-b-2'></div>
+        <h1 className='absolute text-left bottom-0 left-1/4 text-white text-8xl font-semibold backdrop-blur-md w-1/2 h-full bg-white/10 px-4 py-10'>Snap Jam</h1>
       </div>
-      <div className="tab-div">
-        <NavLink to="/" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-          <h1>Home</h1>
-        </NavLink>
-        <NavLink to="/create" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-          <h1>Create</h1>
-        </NavLink>
-        <NavLink to="/projects" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-          <h1>Projects</h1>
-        </NavLink>
-      </div>
+      <div className="nav-bar ">
+        <div className="nav-title-div">
+          <h1 className='text-slate-700 font-bold xl'>Snap Jam</h1>
+          </div>
+          <div className="tab-div">
+          <NavLink to="/" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
+            <h1>Home</h1>
+          </NavLink>
+          <NavLink to="/create" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
+            <h1>Create</h1>
+          </NavLink>
+          <NavLink to="/projects" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
+            <h1>Projects</h1>
+          </NavLink>
+        </div>
       <div className="profile-info-div">
         <h1 className="text-slate-700 font-medium">User: {user?.nickname}</h1>
         <img alt='user profile' className="h-10 rounded-full m-auto mr-4" src={user?.picture}></img> {/* TODO link this to a profile page */}
       </div>
     </div>
+    </>
   )
 }
